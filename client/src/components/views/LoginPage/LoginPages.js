5cd2f4e5be91c9e35e25801655dca86e2451a726
SongGol <dldmsgus2@gmail.com>
2022-04-06 20:43:38 +0900

    :lipstick: 로그인 화면

--- client/src/components/views/LoginPage/LoginPages.js
+++ client/src/components/views/LoginPage/LoginPages.js
@@ -1,59 +1,125 @@
 import { PromiseProvider } from 'mongoose';
-import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
 import { loginUser } from '../../../_actions/user_action';
 import { useNavigate } from 'react-router-dom';
+import useInput from '../../hooks/useInput';
+import { Button, Error, Form, Header, Input, Label, LinkContainer } from '../SignUpPage/styles';
+//import fetcher from '../../utils/fetcher';
+//import axios from 'axios';
+import React, { useCallback, useState } from 'react';
+import { Navigate, Link } from 'react-router-dom';
+//import useSWR from 'swr';
 
 const LoginPage = () => {
-    const [email, setEmail] = useState('');
-    const [password, setPassword] = useState('');
+    //const { data: userData, error, revalidate } = useSWR('/api/users', fetcher);
+    const [loginError, setLoginError] = useState(false);
+    const [email, setEmail, ] = useInput('');
+    const [password, setPassword, ] = useInput('');
 
     let navigate = useNavigate();
     const dispatch = useDispatch();
 
-    const onEmailHandler = (event) => {
-        setEmail(event.currentTarget.value);
-    };
+    const onSubmit = useCallback(
+        (e) => {
+            e.preventDefault();
+            setLoginError(false);
 
-    const onPasswordHandler = (event) => {
-        setPassword(event.currentTarget.value);
-    };
+            let body = {
+                email: email,
+                password: password,
+            };
+            
+            dispatch(loginUser(body))
+                .then(response => {
+                    if (response.payload.loginSuccess) {
+                        navigate('/');
+                    } else {
+                        alert('Error');
+                    }
+                });
+        }, [email, password],
+    );
 
-    const onSubmitHandler = (event) => {
-        event.preventDefault(); //설정을 안하면 화면이 refresh
-
-        let body = {
-            email: email,
-            password: password,
-        };
-
-        dispatch(loginUser(body))
-            .then(response => {
-                if (response.payload.loginSuccess) {
-                    navigate('/');
-                } else {
-                    alert('Error');
-                }
-            })
-
-        
-    };
+    // console.log(error, userData);
+    // if (!error && userData) {
+    //     console.log('로그인 됨', userData);
+    //     return <Navigate to='/workspace/sleact/channel/일반' />;
+    // }
 
     return (
-        <>
-            <div style={{display: 'flex', justifyContent: 'center', alignItems:'center', width: '100%', height: '100vh'}}>
-                <form style={{display: 'flex', flexDirection: 'column'}}
-                    onSubmit={onSubmitHandler}>
-                    <label>Email</label>
-                    <input type="email" value={email} onChange={onEmailHandler}/>
-                    <label>Password</label>
-                    <input type="password" value={password} onChange={onPasswordHandler}/>
-                    <br />
-                    <button type="submit">Login</button>
-                </form>
-            </div>
-        </>
-    )
+        <div id="container">
+            <Header>Sleact</Header>
+            <Form onSubmit={onSubmit}>
+                <Label id="email-label">
+                    <span>이메일 주소</span>
+                    <div>
+                        <Input type="email" id="email" value={email} onChange={setEmail} />
+                    </div>
+                </Label>
+                <Label id="password-label">
+                    <span>비밀번호</span>
+                    <div>
+                        <Input type="password" id="password" name="password" value={password} onChange={setPassword} />
+                    </div>
+                    {loginError && <Error>이메일과 비밀번호 조합이 일치하지 않습니다.</Error>}
+                </Label>
+                <Button type="submit">로그인</Button>
+            </Form>
+            <LinkContainer>
+                아직 회원이 아니신가요?&nbsp;
+                <Link to="/signup">회원가입 하러가기</Link>
+            </LinkContainer>
+        </div>
+    );
+    // const [email, setEmail] = useState('');
+    // const [password, setPassword] = useState('');
+
+    // let navigate = useNavigate();
+    // const dispatch = useDispatch();
+
+    // const onEmailHandler = (event) => {
+    //     setEmail(event.currentTarget.value);
+    // };
+
+    // const onPasswordHandler = (event) => {
+    //     setPassword(event.currentTarget.value);
+    // };
+
+    // const onSubmitHandler = (event) => {
+    //     event.preventDefault(); //설정을 안하면 화면이 refresh
+
+    //     let body = {
+    //         email: email,
+    //         password: password,
+    //     };
+
+    //     dispatch(loginUser(body))
+    //         .then(response => {
+    //             if (response.payload.loginSuccess) {
+    //                 navigate('/');
+    //             } else {
+    //                 alert('Error');
+    //             }
+    //         })
+
+        
+    // };
+
+    // return (
+    //     <>
+    //         <div style={{display: 'flex', justifyContent: 'center', alignItems:'center', width: '100%', height: '100vh'}}>
+    //             <form style={{display: 'flex', flexDirection: 'column'}}
+    //                 onSubmit={onSubmitHandler}>
+    //                 <label>Email</label>
+    //                 <input type="email" value={email} onChange={onEmailHandler}/>
+    //                 <label>Password</label>
+    //                 <input type="password" value={password} onChange={onPasswordHandler}/>
+    //                 <br />
+    //                 <button type="submit">Login</button>
+    //             </form>
+    //         </div>
+    //     </>
+    // )
 };
 
 export default LoginPage;
